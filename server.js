87c7a722ca1998aa4674e7f93b816d54f6c10066
Victor Yang <vyang1222@gmail.com>
2020-11-01 19:07:48 -0600

    update server

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 import express from "express";
-import expressfavicon from "express-favicon";
+import favicon from "express-favicon";
 import path from "path";
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8000;
 const app = express();
 
-app.use(expressfavicon(__dirname + "/build/favicon.ico"));
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, "build")));
+app.use(favicon(__dirname + "/build/favicon.ico"));
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
